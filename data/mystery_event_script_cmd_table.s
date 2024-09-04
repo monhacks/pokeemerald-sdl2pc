9db68c0dbaf4701ff1282296a6ef7d27d8788e4f
NT_x86 <sumoner200@gmail.com>
2024-09-04 15:41:33 +0300

    Fix mystery event cmd table

--- data/mystery_event_script_cmd_table.s
+++ data/mystery_event_script_cmd_table.s
@@ -2,21 +2,21 @@
 
 	.align 2
 gMysteryEventScriptCmdTable::
-	.int MEScrCmd_nop                 /* 0x00*/
-	.int MEScrCmd_checkcompat         /* 0x01*/
-	.int MEScrCmd_end                 /* 0x02*/
-	.int MEScrCmd_setmsg              /* 0x03*/
-	.int MEScrCmd_setstatus           /* 0x04*/
-	.int MEScrCmd_runscript           /* 0x05*/
-	.int MEScrCmd_initramscript       /* 0x06*/
-	.int MEScrCmd_setenigmaberry      /* 0x07*/
-	.int MEScrCmd_giveribbon          /* 0x08*/
-	.int MEScrCmd_givenationaldex     /* 0x09*/
-	.int MEScrCmd_addrareword         /* 0x0a*/
-	.int MEScrCmd_setrecordmixinggift /* 0x0b*/
-	.int MEScrCmd_givepokemon         /* 0x0c*/
-	.int MEScrCmd_addtrainer          /* 0x0d*/
-	.int MEScrCmd_enableresetrtc      /* 0x0e*/
-	.int MEScrCmd_checksum            /* 0x0f*/
-	.int MEScrCmd_crc                 /* 0x10*/
+	.quad MEScrCmd_nop                 /* 0x00*/
+	.quad MEScrCmd_checkcompat         /* 0x01*/
+	.quad MEScrCmd_end                 /* 0x02*/
+	.quad MEScrCmd_setmsg              /* 0x03*/
+	.quad MEScrCmd_setstatus           /* 0x04*/
+	.quad MEScrCmd_runscript           /* 0x05*/
+	.quad MEScrCmd_initramscript       /* 0x06*/
+	.quad MEScrCmd_setenigmaberry      /* 0x07*/
+	.quad MEScrCmd_giveribbon          /* 0x08*/
+	.quad MEScrCmd_givenationaldex     /* 0x09*/
+	.quad MEScrCmd_addrareword         /* 0x0a*/
+	.quad MEScrCmd_setrecordmixinggift /* 0x0b*/
+	.quad MEScrCmd_givepokemon         /* 0x0c*/
+	.quad MEScrCmd_addtrainer          /* 0x0d*/
+	.quad MEScrCmd_enableresetrtc      /* 0x0e*/
+	.quad MEScrCmd_checksum            /* 0x0f*/
+	.quad MEScrCmd_crc                 /* 0x10*/
 gMysteryEventScriptCmdTableEnd::
